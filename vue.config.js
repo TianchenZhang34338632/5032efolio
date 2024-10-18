0b37de6b81d487a93b2984740d3ead5dc83d8108
zhangtianchen@agree.com.cn <zhangtianchen@agree.com.cn>
2024-10-18 22:23:50 +1100

    workflow-config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 // vue.config.js
 module.exports = {
   // publicPath
-  publicPath: process.env.NODE_ENV === 'production' ? '/5032efolio/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/5032efolio/' : '/W11W12',
   
   // outputDir
   outputDir: 'dist'
